6bbf6aa7876990ce5b1fdadccc71ee57c644b487
leisiamedeiros <leisiamedeiros@gmail.com>
2020-09-22 23:39:53 -0300

    iniciado metodos soma e subtracao - metodos do trabalho do modulo 01

--- Modulo1/Calculadora/js/script.js
+++ Modulo1/Calculadora/js/script.js
@@ -1,5 +1,49 @@
-console.log("Olá, mundo!");
+var inputA = undefined;
+var inputB = undefined;
 
-var titulo = document.querySelector("h3");
-titulo.textContent = "Titulo modificado!";
-titulo.classList.add("text-uppercase");
+function start() {
+  inputA = document.querySelector("#ainput");
+  inputB = document.querySelector("#binput");
+
+  startInputs();
+  render();
+}
+
+function startInputs() {
+  inputA.addEventListener("keyup", calculate);
+  inputB.addEventListener("keyup", calculate);
+}
+
+function calculate() {
+  if (inputA.value && inputB.value) {
+    var parsedA = parseInt(inputA.value, 10);
+    var parsedB = parseInt(inputB.value, 10);
+
+    soma(parsedA, parsedB);
+    subtracaoA(parsedA, parsedB);
+    subtracaoB(parsedB, parsedA);
+  }
+}
+
+function soma(a, b) {
+  var result = document.querySelector("#rsoma");
+  result.value = a + b;
+}
+
+function subtracaoA(a, b) {
+  var result = document.querySelector("#rsubtracaoa");
+  result.value = a - b;
+}
+
+function subtracaoB(b, a) {
+  var result = document.querySelector("#rsubtracaob");
+  result.value = b - a;
+}
+
+function formatNumber(value) {
+  return new Intl.NumberFormat("pt-BR").format(value);
+}
+
+function render() {}
+
+start();
